f0b9a3aab908480c17e7c14a367b1e9ad8a93f23
Sumit Rai <sumitrai2019@gmail.com>
2026-07-09 20:07:28 +0530

    adding api keys

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : import.meta.env.VITE_API_URL,
   withCredentials:true,//This is to allow cookies to be sent with requests- everytime
 });
